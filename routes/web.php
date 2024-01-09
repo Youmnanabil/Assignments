22eb93bef5a8957ab5354a44d385699e88fdf29e
youmna <youmnanabil34@gmail.com>
2024-01-10 00:55:14 +0200

    assignment 11

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
-use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,32 +50,34 @@ Route::get('contactus', function(){
 
 
 // assignment day 3
-Route::get('login', function(){
-    return view('login');
-});
-Route::post('/display-form-data',[FormController::class, 'show'])->name('display.form.data');
+// Route::get('login', function(){
+//     return view('login');
+// });
+// Route::post('/display-form-data',[FormController::class, 'show'])->name('display.form.data');
 
 
-// assignment day 4
-Route::get('post', [PostController::class,'create'])->name('post');
-Route::post('storePost', [PostController::class, 'store'])->name('storePost');
+// // assignment day 4
+// Route::get('post', [PostController::class,'create'])->name('post');
+// Route::post('storePost', [PostController::class, 'store'])->name('storePost');
 
-//assignment day 5
-Route::get('postTable', [PostController::class, 'index'])->name('postTable');
-Route::get('editPost/{id}', [PostController::class, 'edit']);
-Route::put('update/{id}',[PostController::class, 'update'])->name('update');
+// //assignment day 5
+// Route::get('postTable', [PostController::class, 'index'])->name('postTable');
+// Route::get('editPost/{id}', [PostController::class, 'edit']);
+// Route::put('update/{id}',[PostController::class, 'update'])->name('update');
 
-//assignment day 6
-Route::get('showPost/{id}', [PostController::class, 'show'])->name('showPost');
-Route::get('deletePost/{id}', [PostController::class, 'destroy'])->name('deletePost');
-Route::get('trashed', [PostController::class, 'trashed'])->name('trashed');
-Route::get('forceDelete/{id}', [PostController::class, 'forceDelete'])->name('forceDelete');
-Route::get('restore/{id}', [PostController::class, 'restore'])->name('restore');
+// //assignment day 6
+// Route::get('showPost/{id}', [PostController::class, 'show'])->name('showPost');
+// Route::get('deletePost/{id}', [PostController::class, 'destroy'])->name('deletePost');
+// Route::get('trashed', [PostController::class, 'trashed'])->name('trashed');
+// Route::get('forceDelete/{id}', [PostController::class, 'forceDelete'])->name('forceDelete');
+// Route::get('restore/{id}', [PostController::class, 'restore'])->name('restore');
 
 //assignment day 11
-Auth::routes(['verify'=>true]);
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 //Route::get('reg', function(){
   //  return view('auth.register');
 //});
+
+Auth::routes(['verify'=>true]);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
